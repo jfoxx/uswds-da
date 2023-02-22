d73e7451607421f9338ac56028fb45727d393561
Jeff Foxx <foxx@adobe.com>
2023-02-22 16:57:58 -0500

    Adjusting font location

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ uswds.paths.dist.theme = './uswds';
 uswds.paths.dist.css = './styles/uswds';
 uswds.paths.dist.js = './scripts';
 uswds.paths.dist.img = './icons';
+uswds.paths.dist.fonts = './fonts';
 
 exports.init = uswds.init;
 exports.compile = uswds.compile;
