81d8ef58680bbc92eeec6875cee123be06af03d9
Jeff Foxx <foxx@adobe.com>
2023-02-27 11:58:39 -0500

    Alert component

---
+++ blocks/alert/alert.js
@@ -0,0 +1,24 @@
+export default function decorate(block) {
+  const classes = block.classList.value.split(' ');
+  if (classes.length > 1) {
+    const alertType = 'usa-alert--' + classes[1];
+    block.classList.add(alertType);
+   }
+  block.classList.add('usa-alert');
+  const titleP = block.querySelector('strong').parentElement;
+  const body = document.createElement('div');
+  const title = document.createElement('h4');
+  title.classList.add('usa-alert__heading');
+  title.innerText = block.querySelector('strong').innerText;
+  body.append(title);
+  titleP.remove();
+  body.classList.add('usa-alert__body');
+  const text = document.createElement('p');
+  text.classList.add('usa-alert__text');
+  var textHTML = block.querySelector('p').innerHTML;
+  text.innerHTML = textHTML;
+  text.querySelector('a').classList.add('usa-link');
+  body.append(text);
+  block.textContent = '';
+  block.append(body);
+}
