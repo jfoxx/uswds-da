ff818170afbaf452f780901b580cdc023e6963d2
Jeff Foxx <foxx@adobe.com>
2023-03-09 12:03:32 -0500

    generate unique id for each accordion (date.now)

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -3,6 +3,8 @@ export default function decorate(block) {
   const items = block.querySelectorAll(':scope > div');
   let itemNumber = 1;
   const collection = document.createElement('div');
+  const uniqueId = Date.now();
+  const uniqueLabel = `accordion-${uniqueId}`;
   items.forEach((i) => {
     const pieces = i.querySelectorAll('div');
     const heading = document.createElement('h4');
@@ -11,12 +13,13 @@ export default function decorate(block) {
     button.classList.add('usa-accordion__button');
     button.setAttribute('type', 'button');
     button.setAttribute('aria-expanded', 'false');
-    button.setAttribute('aria-controls', `a${itemNumber}`);
+    button.setAttribute('aria-controls', `${uniqueLabel}-${itemNumber}`);
     button.innerText = pieces[0].innerText;
     heading.append(button);
     const div = document.createElement('div');
     div.classList.add('usa-accordion__content', 'usa-prose');
-    div.id = `a${itemNumber}`;
+    div.id = `${uniqueLabel}-${itemNumber}`;
+    div.setAttribute('hidden', '');
     div.innerHTML = pieces[1].innerHTML;
     collection.append(heading, div);
     itemNumber += 1;
