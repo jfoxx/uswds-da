0ea8ce2a42392a9d1017096729a00ff1e9f976af
Jeff Foxx <foxx@adobe.com>
2023-03-23 20:50:39 -0400

    detect single or double cell tolerate p or no p

--- blocks/alert/alert.js
+++ blocks/alert/alert.js
@@ -5,25 +5,35 @@ export default function decorate(block) {
     block.classList.add(alertType);
   }
   block.classList.add('usa-alert');
-  const titleP = block.querySelector('strong').parentElement;
+  const content = block.querySelectorAll(':scope > div > div');
   const body = document.createElement('div');
-  const title = document.createElement('h4');
-  title.classList.add('usa-alert__heading');
-  title.innerText = block.querySelector('strong').innerText;
-  body.append(title);
-  titleP.remove();
   body.classList.add('usa-alert__body');
-  const text = document.createElement('p');
-  text.classList.add('usa-alert__text');
-  const textHTML = block.querySelector('p').innerHTML;
-  text.innerHTML = textHTML;
-  const links = text.querySelectorAll('a');
-  if (links) {
-    links.forEach((l) => {
-      l.classList.add('usa-link');
-    });
+  if (content.length > 1) {
+    const title = document.createElement('h4');
+    title.classList.add('usa-alert__heading');
+    title.innerText = content[0].innerText;
+    title.querySelectorAll('br').forEach((i) => { i.remove(); });
+    body.append(title);
+    const text = document.createElement('div');
+    text.innerHTML = content[1].innerHTML;
+    const links = text.querySelectorAll('a');
+    if (links) {
+      links.forEach((l) => {
+        l.classList.add('usa-link');
+      });
+    }
+    body.append(text);
+  } else {
+    const text = document.createElement('div');
+    text.innerHTML = content[0].innerHTML;
+    const links = text.querySelectorAll('a');
+    if (links) {
+      links.forEach((l) => {
+        l.classList.add('usa-link');
+      });
+    }
+    body.append(text);
   }
-  body.append(text);
   block.textContent = '';
   block.append(body);
 }
