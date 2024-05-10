2fa3efe47c9fca1d1c7816152111dc40c916ef50
Jeff Foxx <88052587+jfoxx@users.noreply.github.com>
2024-05-10 10:31:35 -0400

    Update scripts.js
    added DA live preview function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -315,3 +315,10 @@ async function loadPage() {
 }
 
 loadPage();
+
+(async function livePreview() {
+  const preview = new URL(window.location.href).searchParams.get('dapreview');
+  if (!preview) return;
+  const origin = preview === 'local' ? 'http://localhost:3000' : 'https://da.live';
+  import(`${origin}/scripts/dapreview.js`).then(({ default: daPreview }) => daPreview(loadPage));
+}());
