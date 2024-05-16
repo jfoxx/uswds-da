535c0160abdf85eeb7dc9b17dcef2fdadc53cee2
Jeff Foxx <foxx@adobe.com>
2024-05-16 13:24:48 -0400

    fixed logo

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -27,18 +27,33 @@ export default async function decorate(block) {
       if (section) section.classList.add(c);
     });
 
-    // make the navbar
+    //make the logo
     const logo = document.createElement('div');
     logo.className = 'usa-logo';
     logo.id = 'extended-logo';
     const logoContents = block.children[0].innerHTML;
     logo.innerHTML = logoContents;
-    logo.querySelector('p').className = 'usa-logo__text';
-    const siteName = logo.innerText.replace(/(\r\n|\n|\r)/gm, '').trim();
+    const logodiv = logo.querySelector('.logo > div');
+    const siteNameDiv = logodiv.children[1];
+    const siteName = siteNameDiv.innerText.replace(/(\r\n|\n|\r)/gm, '').trim();
     const picture = logo.querySelector('picture');
-    picture.parentNode.className = 'usa-logo__text--img';
     const img = picture.querySelector('img');
     img.setAttribute('aria-label', `Logo for ${siteName}`);
+    const logoEm = document.createElement('em');
+    logoEm.className = 'usa-logo__text';
+    const siteLink = siteNameDiv.querySelector('a');
+    siteLink.setAttribute('title', siteName);
+    const siteUrl = siteLink.href;
+    logoEm.append(siteLink);
+    const logoLink = document.createElement('a');
+    logoLink.href = siteUrl;
+    logoLink.className = 'usa-logo__text--img';
+    logoLink.append(picture);
+    logoEm.prepend(logoLink);
+    logo.innerHTML = '';
+    logo.append(logoEm);
+
+    // make the navbar
     const menuButton = document.createElement('button');
     menuButton.className = 'usa-menu-btn';
     menuButton.setAttribute('type', 'button');
